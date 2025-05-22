d71360cc16eaaf8869d65764c1c94f7d73906517
cosinewaves <elkhaadnan@gmail.com>
2025-05-22 17:36:16 +0100

    fix types

--- motion/init.lua
+++ motion/init.lua
@@ -8,37 +8,24 @@ export type Connection = internalTypings.Connection
 export type MiddlewareHandle = internalTypings.MiddlewareHandle
 export type Signal<T...> = internalTypings.Signal<T...>
 
-export type Motion<T...> = {
-  -- public
-    new: () -> Signal<T...>,
-    Connect: (self: Signal<T...>, callback: (T...) -> ()) -> Connection,
-    Once: (self: Signal<T...>, callback: (T...) -> ()) -> Connection,
-    Wait: (self: Signal<T...>, timeoutSeconds: number?) -> ...any,
-    Fire: (self: Signal<T...>, ...T) -> (),
-    DisconnectAll: (self: Signal<T...>) -> (),
-  -- private
-  __index: Connection,
-
-}
-
-local motion = {} :: Motion<any>
+local motion = {} :: Signal<any>
 motion.__index = motion
 
-function motion.new<T...>(): internalTypings.Signal<T...>
+function motion.new<T...>(): Signal<T...>
     return setmetatable({
-        _head = nil :: internalTypings.Connection?,
-    }, motion) :: any
+        _head = nil :: Connection?,
+    }, motion)
 end
 
-function motion:Connect<T...>(callback: (T...) -> ()): internalTypings.Connection
+function motion:Connect<T...>(callback: (T...) -> ()): Connection
     local conn = connection.new(self, callback)
     conn._next = self._head
     self._head = conn
     return conn
 end
 
-function motion:Once<T...>(callback: (T...) -> ()): internalTypings.Connection
-    local conn: internalTypings.Connection
+function motion:Once<T...>(callback: (T...) -> ()): Connection
+    local conn: Connection
     conn = self:Connect(function(...: T...)
         conn:Disconnect()
         callback(...)
@@ -46,9 +33,9 @@ function motion:Once<T...>(callback: (T...) -> ()): internalTypings.Connection
     return conn
 end
 
-function motion:Wait<T...>(timeoutSeconds: number?): ...any
+function motion:Wait<T...>(timeoutSeconds: number?): ...T
     local thread = coroutine.running()
-    local conn: internalTypings.Connection
+    local conn: Connection
     conn = self:Connect(function(...: T...)
         conn:Disconnect()
         task.spawn(thread, ...)
