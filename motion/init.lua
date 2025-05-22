2469f25f5f4a565c0ecc417ad60ea1d30af76eae
cosinewaves <elkhaadnan@gmail.com>
2025-05-22 17:13:56 +0100

    create signal, connection and internalTypings

--- motion/init.lua
+++ motion/init.lua
@@ -1,2 +1,13 @@
 --!strict
 -- init.lua
+
+local signal = require(script.signal)
+local internalTypings = require(script.internalTypings)
+
+export type Connection = internalTypings.Connection
+export type MiddlewareHandle = internalTypings.MiddlewareHandle
+export type Signal<T...> = internalTypings.Signal<T...>
+
+return {
+  signal = signal,
+}
